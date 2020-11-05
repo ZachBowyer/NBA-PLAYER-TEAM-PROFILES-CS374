ff8bad44b804207d7041ff8806c93c1b13ef1eb9
ZachBowyer <54867730+ZachBowyer@users.noreply.github.com>
2020-11-04 20:42:36 -0800

    Created schema diagram and py file now creates all empty tables
    Created UML for schema plans.
    Main py script now creates empty tables for db file.
    Edited some of the raw data in the csv file. It looks like a lot of changes in the csv file because I sorted the data to check for consitency among potential foreign keys.

--- DB_Creation/main.py
+++ DB_Creation/main.py
@@ -5,8 +5,8 @@ print("Opened/created database successfully")
 
 conn.execute("""CREATE TABLE TeamTotals
             (Abbr TEXT PRIMARY KEY,
-             TEAM VARCHAR(255),
-             GAMES INTEGER,
+             TeamName VARCHAR(255),
+             GamesPlayed INTEGER,
              Minutes INTEGER,
              FGM INTEGER,
              FGA INTEGER,
@@ -31,21 +31,64 @@ conn.execute("""CREATE TABLE TeamTotals
              PTS INTEGER);""")
 print("Table TeamTotals created")
 
-             #3P INTEGER,
-             #3PA INTEGER,
-             #3P_Perc REAL,
-             #2P INTEGER,
-             #2PA INTEGER
-             #2P_Perc REAL,
-             #FT INTEGER,
-             #FTA INTEGER
-             #FT_Perc REAL,
-             #ORD INTEGER,
-             #DRB INTEGER,
-             #TRB INTEGER,
-             #AST INTEGER,
-             #STL INTEGER,
-             #BLK INTEGER,
-             #TOV INTEGER,
-             #PF INTEGER,
-             #PTS INTEGER)
+conn.execute("""CREATE TABLE TeamOppTotals
+            (Abbr TEXT PRIMARY KEY,
+             TeamName VARCHAR(255),
+             GamesPlayed INTEGER,
+             Minutes INTEGER,
+             FGM INTEGER,
+             FGA INTEGER,
+             FG_Perc REAL,
+             ThreePointM INTEGER,
+             ThreePointA INTEGER,
+             ThreePoint_Perc REAL,
+             TwoPointM INTEGER,
+             TwoPointA INTEGER,
+             TwoPoint_Perc REAL,
+             FT INTEGER,
+             FTA INTEGER,
+             FT_Perc REAL,
+             ORB INTEGER,
+             DRB INTEGER,
+             TRB INTEGER,
+             AST INTEGER,
+             STL INTEGER,
+             BLK INTEGER,
+             TOV INTEGER,
+             PF INTEGER,
+             PTS INTEGER);""")
+print("Table TeamOppTotals created")
+
+conn.execute("""CREATE TABLE PlayerTotals
+            (ID PRIMARY KEY,
+             PlayerName VARCHAR(255),
+             Pos TEXT,
+             Age Integer,
+             Team TEXT,
+             G INTEGER,
+             GS INTEGER,
+             MP INTEGER,
+             FGM INTEGER,
+             FGA INTEGER,
+             FG_Perc REAL,
+             ThreePointM INTEGER,
+             ThreePointA INTEGER,
+             ThreePoint_Perc REAL,
+             TwoPointM INTEGER,
+             TwoPointA INTEGER,
+             TwoPoint_Perc REAL,
+             eFG_Perc Real,
+             FT INTEGER,
+             FTA INTEGER,
+             FT_Perc REAL,
+             ORB INTEGER,
+             DRB INTEGER,
+             TRB INTEGER,
+             AST INTEGER,
+             STL INTEGER,
+             BLK INTEGER,
+             TOV INTEGER,
+             PF INTEGER,
+             PTS INTEGER);""")
+print("Table PlayerTotals created")
+
