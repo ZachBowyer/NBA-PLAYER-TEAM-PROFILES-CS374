8e8a27439b0a25361f95d820f84efea06a08fb28
ZachBowyer <54867730+ZachBowyer@users.noreply.github.com>
2020-11-04 22:26:14 -0800

    Main.py now checks if tables exist before trying to create them
    CheckIfTableExists function given a tableName and sqlite3 connection cursor will return a true or false if a table is found in the db

--- DB_Creation/main.py
+++ DB_Creation/main.py
@@ -1,9 +1,73 @@
+import os
 print("Hello world")
 import sqlite3
+import csv
 conn = sqlite3.connect('NBA_Stats.db')
 print("Opened/created database successfully")
+cursor = conn.cursor()
 
-conn.execute("""CREATE TABLE TeamTotals
+os.system('cls')
+#Creates TeamTotals table
+
+#Given a sqlite3 connection cursor object and a table name,
+#will return true if the given table exists
+def checkIfTableExists(cursor, TableName):
+    cursor.execute(""" SELECT count(*) FROM sqlite_master WHERE type='table' AND name='{}' """.format(TableName))
+    return (cursor.fetchone()[0]==1)
+
+
+if(checkIfTableExists(cursor, 'TeamTotals')):
+    print("TeamTotals table exists")
+else:
+    print('Table DNE')
+    conn.execute("""CREATE TABLE TeamTotals
+    (Abbr TEXT PRIMARY KEY,
+    TeamName VARCHAR(255),
+    GamesPlayed INTEGER,
+    Minutes INTEGER,
+    FGM INTEGER,
+    FGA INTEGER,
+    FG_Perc REAL,
+    ThreePointM INTEGER,
+    ThreePointA INTEGER,
+    ThreePoint_Perc REAL,
+    TwoPointM INTEGER,
+    TwoPointA INTEGER,
+    TwoPoint_Perc REAL,
+    FT INTEGER,
+    FTA INTEGER,
+    FT_Perc REAL,
+    ORB INTEGER,
+    DRB INTEGER,
+    TRB INTEGER,
+    AST INTEGER,
+    STL INTEGER,
+    BLK INTEGER,
+    TOV INTEGER,
+    PF INTEGER,
+    PTS INTEGER);""")
+    print("Table TeamTotals created")
+
+'''
+#Populates data into TeamTotals table
+DataList = []
+with open('TeamTotals.csv', newline = '') as csvfile:
+    data = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+
+    for row in data:
+        DataList.append(row)
+
+print(DataList[1])
+print(DataList[1][0])
+print(DataList[1][1])
+print(DataList[1][2])
+'''
+
+#Creates TeamOpponentTotals table
+if(checkIfTableExists(cursor, 'TeamOppTotals')):
+    print("TeamTotals table exists")
+else:
+    conn.execute("""CREATE TABLE TeamOppTotals
             (Abbr TEXT PRIMARY KEY,
              TeamName VARCHAR(255),
              GamesPlayed INTEGER,
@@ -29,37 +93,14 @@ conn.execute("""CREATE TABLE TeamTotals
              TOV INTEGER,
              PF INTEGER,
              PTS INTEGER);""")
-print("Table TeamTotals created")
+    print("Table TeamOppTotals created")
 
-conn.execute("""CREATE TABLE TeamOppTotals
-            (Abbr TEXT PRIMARY KEY,
-             TeamName VARCHAR(255),
-             GamesPlayed INTEGER,
-             Minutes INTEGER,
-             FGM INTEGER,
-             FGA INTEGER,
-             FG_Perc REAL,
-             ThreePointM INTEGER,
-             ThreePointA INTEGER,
-             ThreePoint_Perc REAL,
-             TwoPointM INTEGER,
-             TwoPointA INTEGER,
-             TwoPoint_Perc REAL,
-             FT INTEGER,
-             FTA INTEGER,
-             FT_Perc REAL,
-             ORB INTEGER,
-             DRB INTEGER,
-             TRB INTEGER,
-             AST INTEGER,
-             STL INTEGER,
-             BLK INTEGER,
-             TOV INTEGER,
-             PF INTEGER,
-             PTS INTEGER);""")
-print("Table TeamOppTotals created")
+#Creates PlayerTotals table
 
-conn.execute("""CREATE TABLE PlayerTotals
+if(checkIfTableExists(cursor, 'PlayerTotals')):
+    print("PlayerTotals table exists")
+else:
+    conn.execute("""CREATE TABLE PlayerTotals
             (ID PRIMARY KEY,
              PlayerName VARCHAR(255),
              Pos TEXT,
@@ -90,5 +131,7 @@ conn.execute("""CREATE TABLE PlayerTotals
              TOV INTEGER,
              PF INTEGER,
              PTS INTEGER);""")
-print("Table PlayerTotals created")
+    print("Table PlayerTotals created")
+
+conn.close()
 
