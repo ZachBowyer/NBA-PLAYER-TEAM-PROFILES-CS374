2da746dd13ee32b37b25d7f38ee2f6d330185314
ZachBowyer <54867730+ZachBowyer@users.noreply.github.com>
2020-12-02 23:33:25 -0800

    Reduced lines and added comments to main index.js file

--- User_downloadables/JS_FILES/index.js
+++ User_downloadables/JS_FILES/index.js
@@ -1,8 +1,9 @@
 //Makes a post request to server
 //Expects a string argument
 //Will be used for SQL statements
-//This should never be used for sensitive data
-//Due to the nature of this app, SQL will be handled client-side
+//This should never be used for any application with sensitive data
+//Very vunerable to SQL injections
+//Due to the nature of this app (class project), SQL will be handled client-side
 function SQLPostRequest(SQLString)
 {
     const PageUrl = 'http://localhost:3000';
@@ -18,56 +19,44 @@ function SQLPostRequest(SQLString)
     return result;
 }
 
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//Event listeners
 
-//Search bar
+//Detects when input is entered into the search bar
+//Once input is detected, will attempt to
+//suggest results that should be clicked on
+//If result is clicked, user will be taken to corresponding player/team page
 let userInput = document.getElementById("UserSearch")
 userInput.addEventListener('input', displayUserInput)
 function displayUserInput()
 {
+    //Clear suggestions before attempting to append more
     $("#ResultsDiv").empty();
+
+    //Get suggested results via query
     let data = SQLPostRequest('SELECT DISTINCT Name FROM (SELECT DISTINCT PlayerName AS Name From PlayerTotals UNION Select DISTINCT TeamName AS Name from TeamTotals) WHERE Name LIKE "' + userInput.value + '%"');
 
-    //add to list
-    for(var i = 0; i < 8; i++)
+    //Once suggested results are retreived, display 12 of them
+    for(var i = 0; (i < 12 && i < data.length); i++)
     {
-        if(i == data.length)
-        {
-            break;
-        }
-        if(data[i].Name.includes("\\")) //if its a player
-        {
-            $("#ResultsDiv").append('<li onclick = "goToPage($(this).text());">' + data[i].Name.split("\\")[0]);
-        }
-        else    //if its a team
-        {
-            $("#ResultsDiv").append('<li onclick = goToPage($(this).text());>' + data[i].Name);
-        }
-    }
-    if(userInput.value == "")
-    {
-        $("#ResultsDiv").empty();
+        //If data is a player, go to player page
+        //Or if data is a team, go to the team page
+        if(data[i].Name.includes("\\")) { $("#ResultsDiv").append('<li onclick = "goToPage($(this).text());">' + data[i].Name.split("\\")[0]); }
+        else { $("#ResultsDiv").append('<li onclick = goToPage($(this).text());>' + data[i].Name); }
     }
 }
 
-function goToPage(NameString)
-{
-    let Playerdata = SQLPostRequest('SELECT DISTINCT PlayerName FROM PlayerTotals WHERE PlayerName LIKE "' + NameString + '%"');
-    let Teamdata = SQLPostRequest('SELECT DISTINCT TeamName FROM TeamTotals WHERE TeamName LIKE "' + NameString + '%"');
-    if(Playerdata.length > 0)
-    {
-        goToPlayerPage(NameString)
-    }
-    else if(Teamdata.length > 0)
-    {
-        goToTeamPage(NameString)
-    }
-    else
-    {
-        console.log("No data detected")
-    }
-}
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+//Player and team pictures
 
-//Get player/team urls given the names of the players/teams
+//Given the name of a player,
+//return url of their profile picture
 function getPlayerPictureFromName(playerName)
 {
     let fullPlayerName = SQLPostRequest('SELECT DISTINCT PlayerName FROM PlayerTotals WHERE PlayerName LIKE "' + playerName + '%"');
@@ -75,6 +64,8 @@ function getPlayerPictureFromName(playerName)
     return "https://www.basketball-reference.com/req/202011101/images/players/" + playerSearchString + ".jpg"
 }
 
+//Given the name of a team, 
+//return url of its profile picture
 function getTeamPictureFromName(teamName)
 {
     //Get team abbreviation from name via SQL
@@ -89,16 +80,14 @@ function getTeamPictureFromName(teamName)
     return "https://d2p3bygnnzw9w3.cloudfront.net/req/202010221/tlogo/bbr/" + teamAbbreviation[0].Abbr + ".png"
 }
 
-function createPlayerSearchString(playerName)
-{
-    let searchString = "";
-    let firstname =  playerName.split(" ")[0]
-    let lastname =  playerName.split(" ")[1]
-    searchString = (lastname.slice(0,5) + firstname.slice(0,2) + "01");
-    searchString = searchString.toLowerCase()
-    return searchString;
-}
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+////Functions that change the url of the page
 
+//Will change window location to
+//http://localhost:3000/User_downloadables/HTML_FILES/AllPlayers.html
 function goToAllPlayersPage()
 {
     $.get( "/User_downloadables/HTML_FILES/AllPlayers.html", function( data ) {
@@ -110,6 +99,8 @@ function goToAllPlayersPage()
     });
 }
 
+//Will change window location to
+//http://localhost:3000/User_downloadables/HTML_FILES/AllTeams.html
 function goToAllTeamsPage()
 {
     $.get( "/User_downloadables/HTML_FILES/AllTeams.html", function( data ) {
@@ -121,6 +112,8 @@ function goToAllTeamsPage()
     });
 }
 
+//Will change window location to
+//http://localhost:3000/User_downloadables/HTML_FILES/Downloads.html
 function goToDownloadPage()
 {
     $.get( "/User_downloadables/HTML_FILES/Downloads.html", function( data ) {
@@ -132,18 +125,28 @@ function goToDownloadPage()
     });
 }
 
-function goToHomePage()
+//Goes back to root page url
+function goToHomePage() { window.location.href = "/"; }
+
+//Given the name of a player or team,
+//Changes window to playerInfo.html if a player is detected
+//Changes window to teamInfo.html if a team is detected
+//Uses returned player from query to create the correct url of the player
+function goToPage(NameString)
 {
-    window.location.href = "/";
+    let Playerdata = SQLPostRequest('SELECT DISTINCT PlayerName FROM PlayerTotals WHERE PlayerName LIKE "' + NameString + '%"');
+    let Teamdata = SQLPostRequest('SELECT DISTINCT TeamName FROM TeamTotals WHERE TeamName LIKE "' + NameString + '%"');
+    if(Playerdata.length > 0) { goToPlayerPage(NameString) }
+    else if(Teamdata.length > 0) { goToTeamPage(NameString) }
+    else {console.log("No data detected")}
 }
 
-
-
-//////////////////////////////////////////
-//////////////////////////////////////////
-//////////////////////////////////////////
-//////////////////////////////////////////
-//Player html relevant js
+//Given name of player
+//Changes window location to playerInfo.html
+//Puts player name into url after # character
+//For example, if you were to go to "Lebron James" page, 
+//The url would be:
+//http://localhost:3000/User_downloadables/HTML_FILES/playerInfo.html#LeBron%20James
 function goToPlayerPage(playerName)
 {
     $.get( "/User_downloadables/HTML_FILES/playerInfo.html", function( data ) {
@@ -155,11 +158,12 @@ function goToPlayerPage(playerName)
     });
 }
 
-//////////////////////////////////////////
-//////////////////////////////////////////
-//////////////////////////////////////////
-//////////////////////////////////////////
-//Team html relevant js
+//Given name of team
+//Changes window location to teamInfo.html
+//Puts team name into url after # character
+//For example, if you were to go to "Detroit Pistons" page, 
+//The url would be:
+//http://localhost:3000/User_downloadables/HTML_FILES/TeamInfo.html#Detroit%20Pistons
 function goToTeamPage(teamName)
 {
     $.get( "/User_downloadables/HTML_FILES/TeamInfo.html", function( data ) {
